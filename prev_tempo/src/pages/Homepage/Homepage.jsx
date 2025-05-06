51b29f7b90ed0bc1fc4ab54bfe67ecea5c8b3924
THIAGO DE MOURA MONTEIRO MELO <a95484363@rn.senac.local>
2025-05-06 17:05:02 -0300

    conclusao da atv-prev_tempo

--- prev_tempo/src/pages/Homepage/Homepage.jsx
+++ prev_tempo/src/pages/Homepage/Homepage.jsx
@@ -1,27 +1,33 @@
 import { useState, useRef } from "react";
 import axios from "axios";
-import WeatherInformations from "../../components/WeatherInformations/WheaterInformations";
-
+import WeatherInformations from "../../components/WeatherInformations/WeatherInformations";
+import "./Homepage.css";
+import WeatherInformations5Days from "../../components/WeatherInformations5Days/WeatherInformations5Days";
 
 function Homepage(){
     const inputRef = useRef();
     const [weather, setWeather] = useState();
+    const [weather5Days, setWeather5Days] = useState();
 
     async function searchCity(){
         const city = inputRef.current.value;
         const key = "046c389345ffe15a1b30faddfd22060f";
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}&units=metric&lang=pt_br`;
+        const url5Days = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units=metric&lang=pt_br`
         const apiInfo = await axios.get(url);
+        const apiInfo5Days = await axios.get(url5Days);
         setWeather(apiInfo.data)
+        setWeather5Days(apiInfo5Days.data);
         console.log(apiInfo.data)
     }
     return(
-        <div>
-            <h1> Componente Homepage </h1>
+        <div className="container">
+            <h1> Previsão do Tempo </h1>
             <input ref={inputRef} type="text"  placeholder="Digite a cidade"/>
             <button onClick={searchCity}>Buscar</button>
 
             {weather && <WeatherInformations weather={weather} /> }
+            {weather5Days && <WeatherInformations5Days weather5Days={weather5Days} /> }
 
         </div>
     )
